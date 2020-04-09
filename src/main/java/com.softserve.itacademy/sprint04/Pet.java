e00acadbbe03614a90eb5b2cc7aed779ed86046e
Marinellalala <marina.pyrozhenko@gmail.com>
2020-04-09 19:47:29 +0300

    Added practice task from sprint 4

---
+++ src/main/java/com.softserve.itacademy/sprint04/Pet.java
@@ -0,0 +1,10 @@
+package com.softserve.itacademy.sprint04;
+
+// Question 1
+// Create a Pet class with public fields name of type String
+// and age of type int that are not initialized.
+
+public class Pet {
+    public String name;
+    public int age;
+}
